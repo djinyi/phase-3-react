a2289c85bed3e60a3389bf88e33f28521cb1262b
Daisy Yi <daisyyi@rocketmail.com>
2023-05-02 17:23:16 -0700

    cleaned code

--- src/Edit.js
+++ src/Edit.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { EditText, EditTextarea } from 'react-edit-text';
+import { EditText } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 
 
