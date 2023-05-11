3f482f749042c5fc3d0bfd3516fbbdd44ff730c8
Daisy Yi <daisyyi@rocketmail.com>
2023-05-11 14:17:52 -0700

    fetch creators

--- src/Creators.js
+++ src/Creators.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Creators(){
+    const [creators, setCreators] = useState("")
+
+
+    useEffect(() => {
+        fetch("http://localhost:9292/creators")
+        .then((r) => r.json())
+        .then((data) => setCreators(data))
+    }, [])
+
+    const list = creators.map((creator) => (
+        <p>Creator: {creator.name}</p>
+    ))
+
     return(
-        <p>Hi</p>
+        <p>{list}</p>
     )
 }
 
