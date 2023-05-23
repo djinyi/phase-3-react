15dc59a69e50030fc52e9c8010d5aecd2310c295
Daisy Yi <daisyyi@rocketmail.com>
2023-05-22 19:18:30 -0700

    add styling

--- src/Creators.js
+++ src/Creators.js
@@ -1,13 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 function Creators({list}){
 
     return(
-        <div>
+        <CreatorDetail>
         <h3>List of creators:</h3>
         <div>{list}</div>
-        </div>
+        </CreatorDetail>
     )
 }
 
 export default Creators;
+
+const CreatorDetail = styled.div`
+display-direction:flex;
+flex-direction:column;
+color:black;
+margin: auto;
+text-align: center;
+font-family: "Times New Roman", Times, serif
+h3{
+    font-size: 30px;
+    text-align: center;
+    padding: 25px 45px 25px 45px;
+    background-color: rgba(135, 206, 235, .2);
+}
+`
