b9dada3465376a9c67121c50c17af7fa47c1e32f
Daisy Yi <daisyyi@rocketmail.com>
2023-05-22 19:20:33 -0700

    add styling

--- src/Creators.js
+++ src/Creators.js
@@ -5,7 +5,7 @@ function Creators({list}){
 
     return(
         <CreatorDetail>
-        <h3>List of creators:</h3>
+        <h3>Creators</h3>
         <div>{list}</div>
         </CreatorDetail>
     )
@@ -14,16 +14,16 @@ function Creators({list}){
 export default Creators;
 
 const CreatorDetail = styled.div`
-display-direction:flex;
-flex-direction:column;
-color:black;
-margin: auto;
-text-align: center;
-font-family: "Times New Roman", Times, serif
 h3{
     font-size: 30px;
     text-align: center;
     padding: 25px 45px 25px 45px;
     background-color: rgba(135, 206, 235, .2);
 }
+display-direction:flex;
+flex-direction:column;
+color:black;
+margin: auto;
+text-align: center;
+font-family: "Times New Roman", Times, serif
 `
